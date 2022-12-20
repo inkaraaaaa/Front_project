710eefa258bae49180f3bcfc2bf5a724b1dcef46
Zhanserik <zhylqybaizhanserik@gmail.com>
2022-12-20 12:23:17 +0600

    demo verson php

--- connect.php
+++ connect.php
@@ -3,19 +3,21 @@
     $firstname = $_POST['firstname'];
     $lastname =$_POST['lastname'];
     $email = $_POST['email'];
-    $tel = $_POST['tel'];
-?>
+    $phonenumber = $_POST['phonenumber'];
+    $selectclub = $_POST['selectclub'];
 
-$conn = new mysql('localhost', 'root', '','enrol');
+    $conn = new mysqli('localhost', 'root', ' ','enroll');
     if($conn->connect_error){
         die('Connection failed : ' $conn->connect_error);
     }
     else{
-        $stmt = $conn->prepare("insert into enroll_form (id,firstname,lastname,email,phonenumber) 
-            values(?, ?, ?, ?, ?)");
-        $stmt->bind_param("isssi", $id, $firstname, $lastname, $email, $phonenumber);
+        $stmt = $conn->prepare("insert into enroll_form(id,firstname,lastname,email,phonenumber, selectclub) 
+            values(?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("isssi", $id, $firstname, $lastname, $email, $phonenumber, $selectclub);
         $stmt->execute();
         echo "Registration Successfully";
         $stmt->close();
         $conn->close();
     }
+?>
+
